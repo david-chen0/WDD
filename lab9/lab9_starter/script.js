2d2895e55ff17c87163c9ac107a6e94fca5d57f8
David Chen <2020davidgolf@gmail.com>
2021-11-03 12:34:12 -0700

    Finish lab9

---
+++ lab9/lab9_starter/script.js
@@ -0,0 +1,7 @@
+function toggleTheme() {
+    let body = document.querySelector("body");
+    body.classList.toggle("dark-mode");
+}
+
+let toggleButton = document.getElementById("toggleButton");
+toggleButton.onclick = toggleTheme;
