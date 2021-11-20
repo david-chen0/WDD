06fd8f5a5d880f6df9f5dfba277ea658f95b9ad5
David Chen <2020davidgolf@gmail.com>
2021-11-19 19:46:19 -0800

    Finish hw11

--- hw11/drizzy/assets/script/script.js
+++ hw11/drizzy/assets/script/script.js
@@ -1,32 +1,68 @@
 // s1 outfit
-let s1 = /* get #s1 outfit */
+let s1 = document.getElementById("s1"); /* get the outfit */
 /* the process: */
 s1.onclick = function() {
 	/* 1. hide #body (set display to none)
 	/* 2. call the function you created to hide all the outfits 
 	(remember: get the list of .outfit elements and use a for loop to hide each outfit)
 	/* 3. show #o1 outfit (set display to block)*/
+	document.getElementById("body").style.display = "none";
+	hideOutfits();
+	document.getElementById("o1").style.display = "block";
+};
+
+function hideOutfits() {
+	let outfits = document.getElementsByClassName("outfit");
+	for (let i = 0; i < outfits.length; i++) {
+		outfits[i].style.display = "none";
+	}
 };
 
 // s2 outfit
 /* get #s2 outfit */
 /* repeat process */
+let s2 = document.getElementById("s2");
+s2.onclick = function() {
+	document.getElementById("body").style.display = "none";
+	hideOutfits();
+	document.getElementById("o2").style.display = "block";
+};
 
 // s3 outfit
 /* get #s3 outfit */
 /* repeat process */
+let s3 = document.getElementById("s3");
+s3.onclick = function() {
+	document.getElementById("body").style.display = "none";
+	hideOutfits();
+	document.getElementById("o3").style.display = "block";
+};
 
 // s4 outfit
 /* get #s4 outfit */
 /* repeat process */
+let s4 = document.getElementById("s4");
+s4.onclick = function() {
+	document.getElementById("body").style.display = "none";
+	hideOutfits();
+	document.getElementById("o4").style.display = "block";
+};
 
 // s5 outfit
 /* get #s5 outfit */
 /* repeat process */
+let s5 = document.getElementById("s5");
+s5.onclick = function() {
+	document.getElementById("body").style.display = "none";
+	hideOutfits();
+	document.getElementById("o5").style.display = "block";
+};
 
 // strip outfit
-let strip = /* get #strip-button */
+let strip = document.getElementById("strip-button"); /* get #strip-button */
 strip.onclick = function() {
 	/* call the function you created to hide all the outfits */
 	/* show #body */
+	hideOutfits();
+	document.getElementById("body").display = "block";
 };
